ea3f2429c71b6da681c8b8be23d365915d818c73
김후정 <fujeong00@gmail.com>
2022-10-13 17:28:41 +0900

    chore: project 설정 변경

--- ExoPlayer/app/src/main/kotlin/com/example/exoplayer/ActMain.kt
+++ ExoPlayer/app/src/main/kotlin/com/example/exoplayer/ActMain.kt
@@ -1,15 +1,20 @@
 package com.example.exoplayer
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
 import com.example.exoplayer.databinding.ActMainBinding
 
 class ActMain : AppCompatActivity() {
 
-    private var mBinding = ActMainBinding.inflate(layoutInflater)
+    private lateinit var mBinding: ActMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        mBinding = ActMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
 
         initView()
         setClickListener()
--- ExoPlayer/app/src/main/kotlin/com/example/exoplayer/ActVideo.kt
+++ ExoPlayer/app/src/main/kotlin/com/example/exoplayer/ActVideo.kt
@@ -2,14 +2,18 @@ package com.example.exoplayer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
 import com.example.exoplayer.databinding.ActVideoBinding
 
 class ActVideo : AppCompatActivity() {
 
-    private var mBinding = ActVideoBinding.inflate(layoutInflater)
+    private lateinit var mBinding: ActVideoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        mBinding = ActVideoBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
 
         initView()
         setClickListener()
